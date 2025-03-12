3ffeff2e690cc51f2b34290e5004998ec502cc59
Samad-Azeez <azeezsamad2004@gmail.com>
2025-03-12 14:56:24 +0100

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './theme';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}>
+        <Text style={styles.itemText}>Coffee</Text>
+      </View>
+    </View>
+  );
+}
+
+// styles for the App component
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: theme.colorWhite,
+    justifyContent: 'center',
+    padding: 20,
+  },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: theme.colorCerulean,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  itemText: { fontSize: 18, fontWeight: '200' },
+});
