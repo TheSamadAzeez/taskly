5c4e620eb961630b5e0ace06bd5c254a59be087a
Samad-Azeez <azeezsamad2004@gmail.com>
2025-03-15 17:40:44 +0100

    feat: Implement shopping list functionality with input field and dynamic item rendering

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,63 @@
-import { StyleSheet, View } from "react-native";
+import { ScrollView, StyleSheet, TextInput } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
+import { useState } from "react";
+
+// ShoppingListItemType
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+// initial list of shopping items (mock data)
+const initialList: ShoppingListItemType[] = [
+  { id: "1", name: "coffee" },
+  { id: "2", name: "tea" },
+  { id: "3", name: "sugar", isCompleted: true },
+];
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState(initialList);
+  const [value, setValue] = useState("");
+
+  // function to hans=dle submit submit event
+  const handleSubmit = () => {
+    if (value) {
+      const newShoppingList: ShoppingListItemType[] = [
+        { id: new Date().toTimeString(), name: value },
+        ...shoppingList,
+      ];
+
+      setShoppingList(newShoppingList);
+      setValue("");
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <ShoppingListItem name="coffee" />
-      <ShoppingListItem name="tea" />
-      <ShoppingListItem name="sugar" isCompleted />
-    </View>
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={styles.contentContainer}
+      stickyHeaderIndices={[0]}
+    >
+      {/* input field to add new shopping item */}
+      <TextInput
+        style={styles.textInput}
+        placeholder="E.g. Coffee"
+        value={value}
+        onChangeText={(text) => setValue(text)} // update the value state. we call also just pass in the setValue function e.g onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {/* render the list of shopping items */}
+      {shoppingList.map((item) => (
+        <ShoppingListItem
+          key={item.id}
+          name={item.name}
+          isCompleted={item.isCompleted}
+        />
+      ))}
+    </ScrollView>
   );
 }
 
@@ -17,7 +66,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: theme.colorWhite,
-    justifyContent: "center",
-    padding: 20,
+    // justifyContent: "center",
+    padding: 12,
+  },
+  contentContainer: {
+    paddingBottom: 24,
+  },
+  textInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 10,
+    marginBottom: 12,
+    fontSize: 18,
+    borderRadius: 50,
+    backgroundColor: theme.colorWhite,
   },
 });
