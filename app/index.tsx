241ee02fe5f30229ea57ca2bbdefa7a011f98d10
Samad-Azeez <azeezsamad2004@gmail.com>
2025-03-15 09:50:12 +0100

    feat: Add Counter and Idea screens with navigation links in App component

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,22 @@
 import { StyleSheet, View } from "react-native";
 import { theme } from "../theme";
 import ShoppingListItem from "../components/ShoppingListItem";
+import { Link } from "expo-router";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Link
+        href="/counter"
+        style={{
+          textAlign: "center",
+          marginBottom: 18,
+          textTransform: "capitalize",
+          fontSize: 18,
+        }}
+      >
+        go to counter
+      </Link>
       <ShoppingListItem name="coffee" />
       <ShoppingListItem name="tea" />
       <ShoppingListItem name="sugar" isCompleted />
