4d1324911fd7c5a5251249699f61916c6e90a87f
Samad-Azeez <azeezsamad2004@gmail.com>
2025-03-21 17:38:39 +0100

    feat: prevent duplicate notification channel creation on Android

--- utils/registerForPushNotificationsAsync.ts
+++ utils/registerForPushNotificationsAsync.ts
@@ -2,14 +2,18 @@ import { Platform } from "react-native";
 import * as Device from "expo-device";
 import * as Notifications from "expo-notifications";
 
+// Track if channel is already created
+let channelCreated = false;
+
 export async function registerForPushNotificationsAsync() {
-  if (Platform.OS === "android") {
+  if (Platform.OS === "android" && !channelCreated) {
     await Notifications.setNotificationChannelAsync("default", {
       name: "default",
       importance: Notifications.AndroidImportance.DEFAULT,
       vibrationPattern: [0, 250, 250, 250],
       showBadge: false,
     });
+    channelCreated = true;
   }
 
   if (Device.isDevice) {
